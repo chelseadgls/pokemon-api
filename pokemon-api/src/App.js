56fffdde6b209c69b18ffc7b345aa73b721cd89b
chelseadgls <chelseamdouglas@gmail.com>
2022-10-28 11:34:17 -0400

    complete

--- pokemon-api/src/App.js
+++ pokemon-api/src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import APIData from './components/APIData';
+import { useState } from 'react';
 
 function App() {
+
   return (
     <div className="App">
       <APIData />
--- pokemon-api/src/components/APIData.js
+++ pokemon-api/src/components/APIData.js
@@ -1,32 +1,67 @@
 import { useState, useEffect } from 'react'
+import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa'
 
-function APIData(props) {
-
+function APIData({ props }) {
   const [pokemon, setPokemon] = useState([])
+  const [current, setCurrent] = useState(0)
+  const length = pokemon.length
+
   const getPokemon = async () => {
     const response = await fetch("https://dummydata.netlify.app/pokedex.json")
     const json = await response.json()
     setPokemon(json.pokemon)
   }
 
-  useEffect(() => {}, [])
+  useEffect(() => {
+    getPokemon()
+  }, [])
 
-  if (pokemon.length <= 0) {
-    return <><h1>There is no pokemon</h1>
-      <button onClick={getPokemon}>Get Pokemon</button>
-      </>
+  // if (pokemon.length <= 0) {
+  //   return <><h1>There is no pokemon</h1>
+  //     <button onClick={getPokemon}>Get Pokemon</button>
+  //     </>
+  // }
+
+  // return <div className="pokemon">
+  //   {pokemon.map((poke) => {
+  //     return (
+  //       <div key={poke.id}>
+  //       <h1>{poke.name}</h1>
+  //       <img src={poke.img} alt={poke.name} />
+  //     </div>
+  //     )
+  //   })}
+  //   </div>
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1)
   }
 
-  return <div className="pokemon">
-    {pokemon.map((poke) => {
-      return (
-        <div key={poke.id}>
-        <h1>{poke.name}</h1>
-        <img src={poke.img} alt={poke.name} />
-      </div>
+  const previousSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1)
+  }
+
+  if (!Array.isArray(pokemon) || pokemon.length <= 0) {
+    return null
+  }
+
+  return (
+    <section className="slider">
+      <FaArrowAltCircleLeft className="left-arrow" onClick={previousSlide}  />
+      <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
+      {pokemon.map((poke, index) => {
+        return (
+          <div className={index === current ? 'slide active' : 'slide'} key={index}>
+            {index === current && (
+              <>
+              <img src={poke.img} />
+              <p>{poke.name}</p>
+              </>)}
+          </div>
       )
     })}
-    </div>
+    </section>
+  )
 }
 
 export default APIData
